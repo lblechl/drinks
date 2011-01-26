1abd5459afa19b0bc57467ea28b60b25e651868a
lblechl <lblechl@stu.matcmadison.edu>
2011-01-25 19:14:19 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drinks::Application.config.secret_token = 'e9e9f4c4f3b7ec23ae741e4fa51220374d578a3e3c51233eea5d8e2178cc1b438c4b5848d41202b5943f2de6dbd06eee03b1afc01d0a57a269aa762798871519'
